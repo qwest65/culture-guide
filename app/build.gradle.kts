d5da0a7089a7cd575603d87cba57663ad1478e1d
qwest65 <59307535+qwest65@users.noreply.github.com>
2026-09-22 22:23:09 +0500

    Set app version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "ru.cultureguide"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.6.1"
         buildConfigField("String", "MAPKIT_API_KEY", "\"$mapkitApiKey\"")
     }
     buildFeatures { buildConfig = true }
