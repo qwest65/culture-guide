3f5cfe5f853ae279cd0784e2f671e6c9a1544427
qwest65 <59307535+qwest65@users.noreply.github.com>
2026-09-23 11:29:03 +0500

    Remove unavailable Compose icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,5 +42,4 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:1.8.3")
     implementation("androidx.compose.runtime:runtime:1.8.3")
     implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.material:material-icons-extended:1.8.3")
 }
