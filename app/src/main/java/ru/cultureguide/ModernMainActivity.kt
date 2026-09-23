92e4004b8126fd239190cba3d7c0e0fa06961c7b
qwest65 <59307535+qwest65@users.noreply.github.com>
2026-09-23 13:38:44 +0500

    Fix pedestrian route distance property

--- app/src/main/java/ru/cultureguide/ModernMainActivity.kt
+++ app/src/main/java/ru/cultureguide/ModernMainActivity.kt
@@ -338,7 +338,7 @@ class ModernMainActivity : ComponentActivity() {
                         zIndex = 6f
                     }
                     routePolylines += line
-                    val legDistanceMeters = result[0].metadata.weight.distance.value
+                    val legDistanceMeters = result[0].metadata.weight.walkingDistance.value
                     routeDistanceMeters += legDistanceMeters
                     statusText = "Строю пешеходный маршрут… ${index + 1}/${requestPoints.size - 1} · %.2f км".format(
                         Locale.US, routeDistanceMeters / 1000.0
